79b09fa2e35a5ff4850ef62530471e54c5c9f630
metrixir <admin@metrix.ir>
2018-12-26 13:01:16 +0330

    init project

---
+++ index.js
@@ -0,0 +1,26 @@
+let MetrixUtil = require("./src/metrix-util.js");
+
+module.exports = {
+	initialize: MetrixUtil.initialize,
+	enableLocationListening: MetrixUtil.enableLocationListening,
+	disableLocationListening: MetrixUtil.disableLocationListening,
+	setEventUploadThreshold: MetrixUtil.setEventUploadThreshold,
+	setEventUploadMaxBatchSize: MetrixUtil.setEventUploadMaxBatchSize,
+	setEventMaxCount: MetrixUtil.setEventMaxCount,
+	setEventUploadPeriodMillis: MetrixUtil.setEventUploadPeriodMillis,
+	setSessionTimeoutMillis: MetrixUtil.setSessionTimeoutMillis,
+	setOptOut: MetrixUtil.setOptOut,
+	enableLogging: MetrixUtil.enableLogging,
+	setLogLevel: MetrixUtil.setLogLevel,
+	setOffline: MetrixUtil.setOffline,
+	setFlushEventsOnClose: MetrixUtil.setFlushEventsOnClose,
+	newEvent: MetrixUtil.newEvent,
+	newBusinessEvent: MetrixUtil.newBusinessEvent,
+	screenDisplayed: MetrixUtil.screenDisplayed,
+	setScreenFlowsAutoFill: MetrixUtil.setScreenFlowsAutoFill,
+	setMetrixApiKey: MetrixUtil.setMetrixApiKey,
+	isOptedOut: MetrixUtil.isOptedOut,
+	setDefaultTracker: MetrixUtil.setDefaultTracker,
+	getSessionNum: MetrixUtil.getSessionNum,
+	isScreenFlowsAutoFill: MetrixUtil.isScreenFlowsAutoFill
+};
---
+++ src/metrix-util.js
@@ -0,0 +1,73 @@
+let Metrix = require("react-native").NativeModules.MetrixReactNative;
+import { Platform } from "react-native";
+
+if (Platform.OS === "android") {
+	module.exports = {
+		initialize: function(appKey) {
+			Metrix.initialize(appKey, "0.0.1");
+		},
+		enableLocationListening: function() {
+			Metrix.enableLocationListening();
+		},
+		disableLocationListening: function() {
+			Metrix.disableLocationListening();
+		},
+		setEventUploadThreshold: function(eventUploadThreshold) {
+			Metrix.setEventUploadThreshold(eventUploadThreshold);
+		},
+		setEventUploadMaxBatchSize: function(eventUploadMaxBatchSize) {
+			Metrix.setEventUploadMaxBatchSize(eventUploadMaxBatchSize);
+		},
+		setEventMaxCount: function(eventMaxCount) {
+			Metrix.setEventMaxCount(eventMaxCount);
+		},
+		setEventUploadPeriodMillis: function(eventUploadPeriodMillis) {
+			Metrix.setEventUploadPeriodMillis(eventUploadPeriodMillis);
+		},
+		setSessionTimeoutMillis: function(sessionTimeoutMillis) {
+			Metrix.setSessionTimeoutMillis(sessionTimeoutMillis);
+		},
+		setOptOut: function(optOut) {
+			Metrix.setOptOut(optOut);
+		},
+		enableLogging: function(enableLogging) {
+			Metrix.enableLogging(enableLogging);
+		},
+		setLogLevel: function(logLevel) {
+			Metrix.setLogLevel(logLevel);
+		},
+		setOffline: function(offline) {
+			Metrix.setOffline(offline);
+		},
+		setFlushEventsOnClose: function(flushEventsOnClose) {
+			Metrix.setFlushEventsOnClose(flushEventsOnClose);
+		},
+		newEvent: function(eventName) {
+			Metrix.newEvent(eventName);
+		},
+		newBusinessEvent: function(itemType, itemId, cartType, transactionNum, amount) {
+			Metrix.newBusinessEvent(itemType, itemId, cartType, transactionNum, amount);
+		},
+		screenDisplayed: function(screenName) {
+			Metrix.screenDisplayed(screenName);
+		},
+		setScreenFlowsAutoFill: function(screenFlowsAutoSend) {
+			Metrix.setScreenFlowsAutoFill(screenFlowsAutoSend);
+		},
+		setMetrixApiKey: function(newApiKey) {
+			Metrix.setMetrixApiKey(newApiKey);
+		},
+		isOptedOut: function(trackerToken) {
+			Metrix.isOptedOut(trackerToken);
+		},
+		setDefaultTracker: function(callback) {
+			Metrix.setDefaultTracker(callback);
+		},
+		getSessionNum: function(callback) {
+			Metrix.getSessionNum(callback);
+		},
+		isScreenFlowsAutoFill: function(callback) {
+			Metrix.isScreenFlowsAutoFill(callback);
+		}
+	};
+}
