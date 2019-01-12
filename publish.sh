3c73c84feca379417d5b4d14cc088064dd37f087
Milad Heydari <miladheydari2011@gmail.com>
2019-01-12 13:40:03 +0330

    add publish script

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+VERSION_NAME="0.0.2"
+VERSION_CODE=2
+sed -i '' 's/\"version\".*/\"version\" :\"'"${VERSION_NAME}"'\",/' package.json &
+sed -i '' 's/Metrix.initialize.*/Metrix.initialize(appKey, \"'"${VERSION_NAME}"'\");/' ./src/metrix-util.js &
+sed -i '' 's/versionCode.*/versionCode '"${VERSION_CODE}"'/' ./android/build.gradle &
+sed -i '' 's/versionName.*/versionName \"'"${VERSION_NAME}"'\"/' ./android/build.gradle &
+git add --all &
+git commit -m "v${VERSION_NAME}" &
+git push origin master -f &
+git tag v${VERSION_NAME} &
+git push origin v${VERSION_NAME} &
+npm publish
