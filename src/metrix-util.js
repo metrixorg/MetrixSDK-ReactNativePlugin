84e0d78fead61fa93ad642f065767d7249193d3d
Milad Heydari <miladheydari2011@gmail.com>
2019-01-30 17:51:16 +0330

    v0.04

--- src/metrix-util.js
+++ src/metrix-util.js
@@ -4,7 +4,7 @@ import { Platform } from "react-native";
 if (Platform.OS === "android") {
 	module.exports = {
 		initialize: function(appKey) {
-			Metrix.initialize(appKey, "0.0.3");
+			Metrix.initialize(appKey, "0.0.4");
 		},
 		enableLocationListening: function() {
 			Metrix.enableLocationListening();
