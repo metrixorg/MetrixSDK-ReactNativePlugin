549181f01311efe55fb735d354fdaab8cd3065da
Milad Heydari <miladheydari2011@gmail.com>
2019-08-05 17:49:18 +0430

    use metrixConfig

--- index.js
+++ index.js
@@ -1,27 +1,105 @@
 let MetrixUtil = require("./src/metrix-util.js");
 
-module.exports = {
-	initialize: MetrixUtil.initialize,
-	enableLocationListening: MetrixUtil.enableLocationListening,
-	disableLocationListening: MetrixUtil.disableLocationListening,
-	setEventUploadThreshold: MetrixUtil.setEventUploadThreshold,
-	setEventUploadMaxBatchSize: MetrixUtil.setEventUploadMaxBatchSize,
-	setEventMaxCount: MetrixUtil.setEventMaxCount,
-	setEventUploadPeriodMillis: MetrixUtil.setEventUploadPeriodMillis,
-	setSessionTimeoutMillis: MetrixUtil.setSessionTimeoutMillis,
-	enableLogging: MetrixUtil.enableLogging,
-	setLogLevel: MetrixUtil.setLogLevel,
-	setFlushEventsOnClose: MetrixUtil.setFlushEventsOnClose,
-	newEvent: MetrixUtil.newEvent,
-	newRevenue: MetrixUtil.newRevenue,
-	screenDisplayed: MetrixUtil.screenDisplayed,
-	setScreenFlowsAutoFill: MetrixUtil.setScreenFlowsAutoFill,
-	setMetrixApiKey: MetrixUtil.setMetrixApiKey,
-	setDefaultTracker: MetrixUtil.setDefaultTracker,
-	getSessionNum: MetrixUtil.getSessionNum,
-	isScreenFlowsAutoFill: MetrixUtil.isScreenFlowsAutoFill,
-	addUserAttributes: MetrixUtil.addUserAttributes,
-	setUserMetrics: MetrixUtil.setUserMetrics,
-	setAppSecret: MetrixUtil.setAppSecret,
-	setOnAttributionChangedListener:MetrixUtil.setOnAttributionChangedListener
+var Metrix = {};
+Metrix.onCreate = MetrixUtil.onCreate;
+Metrix.newEvent = MetrixUtil.newEvent;
+Metrix.newRevenue = MetrixUtil.newRevenue;
+Metrix.screenDisplayed = MetrixUtil.screenDisplayed;
+Metrix.addUserAttributes = MetrixUtil.addUserAttributes;
+Metrix.addUserMetrics = MetrixUtil.addUserMetrics;
+Metrix.appWillOpenUrl = MetrixUtil.appWillOpenUrl;
+
+
+var MetrixConfig = function(appId) {
+  this.appId = appId;
+  this.shouldLunchDeeplink = false;
 };
+
+MetrixConfig.prototype.setAppSecret = function(
+  secretId,
+  info1,
+  info2,
+  info3,
+  info4
+) {
+  this.appSecret = {
+    secretId: secretId.toString(),
+    info1: info1.toString(),
+    info2: info2.toString(),
+    info3: info3.toString(),
+    info4: info4.toString()
+  };
+};
+
+MetrixConfig.prototype.setShouldLaunchDeeplink = function(
+  shouldLunchDeeplink
+) {
+  this.shouldLunchDeeplink = shouldLunchDeeplink;
+};
+
+MetrixConfig.prototype.setLocationListening = function(locationListening) {
+  this.locationListening = locationListening;
+};
+
+MetrixConfig.prototype.setEventUploadThreshold = function(
+  eventUploadThreshold
+) {
+  this.eventUploadThreshold = eventUploadThreshold;
+};
+
+MetrixConfig.prototype.setEventUploadMaxBatchSize = function(
+  eventUploadMaxBatchSize
+) {
+  this.eventUploadMaxBatchSize = eventUploadMaxBatchSize;
+};
+
+MetrixConfig.prototype.setEventMaxCount = function(eventMaxCount) {
+  this.eventMaxCount = eventMaxCount;
+};
+
+MetrixConfig.prototype.setEventUploadPeriodMillis = function(
+  eventUploadPeriodMillis
+) {
+  this.eventUploadPeriodMillis = eventUploadPeriodMillis;
+};
+
+MetrixConfig.prototype.setSessionTimeoutMillis = function(
+  sessionTimeoutMillis
+) {
+  this.sessionTimeoutMillis = sessionTimeoutMillis;
+};
+
+MetrixConfig.prototype.setEnableLogging = function(enableLogging) {
+  this.enableLogging = enableLogging;
+};
+
+MetrixConfig.prototype.setLogLevel = function(logLevel) {
+  this.logLevel = logLevel;
+};
+
+MetrixConfig.prototype.setFlushEventsOnClose = function(flushEventsOnClose) {
+  this.flushEventsOnClose = flushEventsOnClose;
+};
+
+MetrixConfig.prototype.setDefaultTrackerToken = function(defaultTrackerToken) {
+  this.defaultTrackerToken = defaultTrackerToken;
+};
+
+MetrixConfig.prototype.setStore = function(store) {
+  this.store = store;
+};
+
+MetrixConfig.prototype.setOnAttributionChangeListener = function(
+  onAttributionChangeListener
+) {    
+  this.onAttributionChangeListener = onAttributionChangeListener;
+};
+
+MetrixConfig.prototype.setOnDeeplinkResponseListener = function(
+  onDeeplinkResponseListener
+) {
+  this.onDeeplinkResponseListener = onDeeplinkResponseListener;
+};
+
+
+module.exports = { Metrix, MetrixConfig };
--- src/metrix-util.js
+++ src/metrix-util.js
@@ -2,171 +2,130 @@ let Metrix = require("react-native").NativeModules.MetrixReactNative;
 import { Platform } from "react-native";
 
 if (Platform.OS === "android") {
-	module.exports = {
-		initialize: function(appKey) {
-			Metrix.initialize(appKey, "0.11.0");
-		},
-		enableLocationListening: function() {
-			Metrix.enableLocationListening();
-		},
-		disableLocationListening: function() {
-			Metrix.disableLocationListening();
-		},
-		setEventUploadThreshold: function(eventUploadThreshold) {
-			Metrix.setEventUploadThreshold(eventUploadThreshold);
-		},
-		setEventUploadMaxBatchSize: function(eventUploadMaxBatchSize) {
-			Metrix.setEventUploadMaxBatchSize(eventUploadMaxBatchSize);
-		},
-		setEventMaxCount: function(eventMaxCount) {
-			Metrix.setEventMaxCount(eventMaxCount);
-		},
-		setAppSecret: function(secretId, info1, info2, info3, info4) {
-			Metrix.setAppSecret(secretId, info1, info2, info3, info4);
-		},
-		setEventUploadPeriodMillis: function(eventUploadPeriodMillis) {
-			Metrix.setEventUploadPeriodMillis(eventUploadPeriodMillis);
-		},
-		setSessionTimeoutMillis: function(sessionTimeoutMillis) {
-			Metrix.setSessionTimeoutMillis(sessionTimeoutMillis);
-		},
-		enableLogging: function(enableLogging) {
-			Metrix.enableLogging(enableLogging);
-		},
-		setLogLevel: function(logLevel) {
-			Metrix.setLogLevel(logLevel);
-		},
-		setFlushEventsOnClose: function(flushEventsOnClose) {
-			Metrix.setFlushEventsOnClose(flushEventsOnClose);
-		},
-		newEvent: function(eventName, customAttributes, customMetrics) {
-				if (customAttributes && customMetrics) {
-				Metrix.newEventCustom(eventName,customAttributes,customMetrics);
-			}
-			else{	
-			Metrix.newEvent(eventName);
-			}
-		},
-		newRevenue: function(slug, revenue, currency, orderId) {
-			let cr = null;
-			if (currency === 0) { 
-				cr = "IRR";
-			} else if(currency === 1) {
-				cr = "USD";
-			} else if (currency == 2) {
-				cr = "EUR";
-			}
-			if (cr == null && orderId == null) {
-				Metrix.newRevenueSimple(slug, revenue);
-			} else if(cr == null && orderId != null) {
-				Metrix.newRevenueOrderId(slug, revenue, orderId);
-			} else if (orderId == null) { 
-				Metrix.newRevenueCurrency(slug, revenue, cr);
-			} else {
-				Metrix.newRevenueFull(slug, revenue, cr, orderId);
-			}
-		},
-		screenDisplayed: function(screenName) {
-			Metrix.screenDisplayed(screenName);
-		},
-		setScreenFlowsAutoFill: function(screenFlowsAutoSend) {
-			Metrix.setScreenFlowsAutoFill(screenFlowsAutoSend);
-		},
-		setMetrixApiKey: function(newApiKey) {
-			Metrix.setMetrixApiKey(newApiKey);
-		},
-		setDefaultTracker: function(callback) {
-			Metrix.setDefaultTracker(callback);
-		},
-		getSessionNum: function(callback) {
-			Metrix.getSessionNum(callback);
-		},
-		isScreenFlowsAutoFill: function(callback) {
-			Metrix.isScreenFlowsAutoFill(callback);
-		},
-		setUserMetrics: function(userMetrics){
-			Metrix.setUserMetrics(userMetrics);
-		},
-		addUserAttributes: function(userAttributes){
-			Metrix.addUserAttributes(userAttributes);
-		},
-		setOnAttributionChangedListener: function(callback){
-			Metrix.setOnAttributionChangedListener(callback);
-		}
-	};
-} else if (Platform.OS === "ios") {
-	module.exports = {
-		initialize: function(appKey) {
-			Metrix.initialize(appKey);
-		},
-		enableLocationListening: function() {
-			// Metrix.enableLocationListening();
-		},
-		disableLocationListening: function() {
-			// Metrix.disableLocationListening();
-		},
-		setEventUploadThreshold: function(eventUploadThreshold) {
-			// Metrix.setEventUploadThreshold(eventUploadThreshold);
-		},
-		setEventUploadMaxBatchSize: function(eventUploadMaxBatchSize) {
-			// Metrix.setEventUploadMaxBatchSize(eventUploadMaxBatchSize);
-		},
-		setEventMaxCount: function(eventMaxCount) {
-			// Metrix.setEventMaxCount(eventMaxCount);
-		},
-		setEventUploadPeriodMillis: function(eventUploadPeriodMillis) {
-			// Metrix.setEventUploadPeriodMillis(eventUploadPeriodMillis);
-		},
-		setSessionTimeoutMillis: function(sessionTimeoutMillis) {
-			// Metrix.setSessionTimeoutMillis(sessionTimeoutMillis);
-		},
-		enableLogging: function(enableLogging) {
-			// Metrix.enableLogging(enableLogging);
-		},
-		setLogLevel: function(logLevel) {
-			// Metrix.setLogLevel(logLevel);
-		},
-		setFlushEventsOnClose: function(flushEventsOnClose) {
-			// Metrix.setFlushEventsOnClose(flushEventsOnClose);
-		},
-		newEvent: function(eventName, customAttributes, customMetrics) {
-				if (customAttributes && customMetrics) {
-				Metrix.newEventCustom(eventName,customAttributes,customMetrics);
-			}
-			else{	
-			Metrix.newEvent(eventName);
-			}
-		},
-		newRevenue: function(slug, revenue, currency, orderId) {
-			// Metrix.newBusinessEvent(itemType, itemId, cartType, transactionNum, amount);
-		},
-		screenDisplayed: function(screenName) {
-			// Metrix.screenDisplayed(screenName);
-		},
-		setScreenFlowsAutoFill: function(screenFlowsAutoSend) {
-			// Metrix.setScreenFlowsAutoFill(screenFlowsAutoSend);
-		},
-		setMetrixApiKey: function(newApiKey) {
-			// Metrix.setMetrixApiKey(newApiKey);
-		},
-		setDefaultTracker: function(callback) {
-			// Metrix.setDefaultTracker(callback);
-		},
-		getSessionNum: function(callback) {
-			// Metrix.getSessionNum(callback);
-		},
-		isScreenFlowsAutoFill: function(callback) {
-			// Metrix.isScreenFlowsAutoFill(callback);
-		},
-		setUserMetrics: function(userMetrics){
-			// Metrix.setUserMetrics(userMetrics);
-		},
-		addUserAttributes: function(userAttributes){
-			// Metrix.addUserAttributes(userAttributes);
-		},
-		setOnAttributionChangedListener: function(callback){
-			// Metrix.setOnAttributionChangedListener(callback);
-		}
-	};
-}
+  module.exports = {
+    onCreate: function(metrixConfig) {
+      Metrix.onCreate(
+        metrixConfig,
+        metrixConfig.shouldLunchDeeplink,
+        metrixConfig.onAttributionChangeListener
+          ? metrixConfig.onAttributionChangeListener
+          : function(e) {},
+        metrixConfig.onDeeplinkResponseListener
+          ? metrixConfig.onDeeplinkResponseListener
+          : function(e) {}
+      );
+    },
+    appWillOpenUrl: function(uri) {
+      Metrix.appWillOpenUrl(uri);
+    },
+    newEvent: function(eventName, customAttributes, customMetrics) {
+      if (customAttributes && customMetrics) {
+        Metrix.newEventCustom(eventName, customAttributes, customMetrics);
+      } else {
+        Metrix.newEvent(eventName);
+      }
+    },
+    newRevenue: function(slug, revenue, currency, orderId) {
+      let cr = null;
+      if (currency === 0) {
+        cr = "IRR";
+      } else if (currency === 1) {
+        cr = "USD";
+      } else if (currency == 2) {
+        cr = "EUR";
+      }
+      if (cr == null && orderId == null) {
+        Metrix.newRevenueSimple(slug, revenue);
+      } else if (cr == null && orderId != null) {
+        Metrix.newRevenueOrderId(slug, revenue, orderId);
+      } else if (orderId == null) {
+        Metrix.newRevenueCurrency(slug, revenue, cr);
+      } else {
+        Metrix.newRevenueFull(slug, revenue, cr, orderId);
+      }
+    },
+    screenDisplayed: function(screenName) {
+      Metrix.screenDisplayed(screenName);
+    },
 
+    addUserMetrics: function(userMetrics) {
+      Metrix.addUserMetrics(userMetrics);
+    },
+    addUserAttributes: function(userAttributes) {
+      Metrix.addUserAttributes(userAttributes);
+    }
+  };
+} else if (Platform.OS === "ios") {
+  module.exports = {
+    initialize: function(appKey) {
+      Metrix.initialize(appKey);
+    },
+    enableLocationListening: function() {
+      // Metrix.enableLocationListening();
+    },
+    disableLocationListening: function() {
+      // Metrix.disableLocationListening();
+    },
+    setEventUploadThreshold: function(eventUploadThreshold) {
+      // Metrix.setEventUploadThreshold(eventUploadThreshold);
+    },
+    setEventUploadMaxBatchSize: function(eventUploadMaxBatchSize) {
+      // Metrix.setEventUploadMaxBatchSize(eventUploadMaxBatchSize);
+    },
+    setEventMaxCount: function(eventMaxCount) {
+      // Metrix.setEventMaxCount(eventMaxCount);
+    },
+    setEventUploadPeriodMillis: function(eventUploadPeriodMillis) {
+      // Metrix.setEventUploadPeriodMillis(eventUploadPeriodMillis);
+    },
+    setSessionTimeoutMillis: function(sessionTimeoutMillis) {
+      // Metrix.setSessionTimeoutMillis(sessionTimeoutMillis);
+    },
+    enableLogging: function(enableLogging) {
+      // Metrix.enableLogging(enableLogging);
+    },
+    setLogLevel: function(logLevel) {
+      // Metrix.setLogLevel(logLevel);
+    },
+    setFlushEventsOnClose: function(flushEventsOnClose) {
+      // Metrix.setFlushEventsOnClose(flushEventsOnClose);
+    },
+    newEvent: function(eventName, customAttributes, customMetrics) {
+      if (customAttributes && customMetrics) {
+        Metrix.newEventCustom(eventName, customAttributes, customMetrics);
+      } else {
+        Metrix.newEvent(eventName);
+      }
+    },
+    newRevenue: function(slug, revenue, currency, orderId) {
+      // Metrix.newBusinessEvent(itemType, itemId, cartType, transactionNum, amount);
+    },
+    screenDisplayed: function(screenName) {
+      // Metrix.screenDisplayed(screenName);
+    },
+    setScreenFlowsAutoFill: function(screenFlowsAutoSend) {
+      // Metrix.setScreenFlowsAutoFill(screenFlowsAutoSend);
+    },
+    setMetrixApiKey: function(newApiKey) {
+      // Metrix.setMetrixApiKey(newApiKey);
+    },
+    setDefaultTracker: function(callback) {
+      // Metrix.setDefaultTracker(callback);
+    },
+    getSessionNum: function(callback) {
+      // Metrix.getSessionNum(callback);
+    },
+    isScreenFlowsAutoFill: function(callback) {
+      // Metrix.isScreenFlowsAutoFill(callback);
+    },
+    setUserMetrics: function(userMetrics) {
+      // Metrix.setUserMetrics(userMetrics);
+    },
+    addUserAttributes: function(userAttributes) {
+      // Metrix.addUserAttributes(userAttributes);
+    },
+    setOnAttributionChangedListener: function(callback) {
+      // Metrix.setOnAttributionChangedListener(callback);
+    }
+  };
+}
