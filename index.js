66b43b273a7e45386e3cf62eb1d0ef2628458655
Milad Heydari <miladheydari2011@gmail.com>
2019-10-08 16:25:27 +0330

    Squashed commit of the following:
    commit 7f41bc3dab8efbf1f4c47a4d9c59ede3c375c1e3
    Author: Milad Heydari <miladheydari2011@gmail.com>
    Date:   Tue Oct 8 14:16:26 2019 +0330
    
        use event emitter for callbacks

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let MetrixUtil = require("./src/metrix-util.js");
+let MetrixUtil = require('./src/metrix-util.js');
+import {DeviceEventEmitter} from 'react-native';
 
 var Metrix = {};
 Metrix.onCreate = MetrixUtil.onCreate;
@@ -9,10 +10,27 @@ Metrix.addUserAttributes = MetrixUtil.addUserAttributes;
 Metrix.addUserMetrics = MetrixUtil.addUserMetrics;
 Metrix.appWillOpenUrl = MetrixUtil.appWillOpenUrl;
 
-
 var MetrixConfig = function(appId) {
-  this.appId = appId;
-  this.shouldLunchDeeplink = false;
+  this.settings = {};
+  this.settings.appId = appId;
+  this.settings.shouldLunchDeeplink = false;
+  this.onAttributionChangeListener = function(e) {};
+  this.onDeeplinkResponseListener = function(e) {};
+  this.onReceiveUserIdListener = function(e) {};
+  this.onSessionIdListener = function(e) {};
+
+  DeviceEventEmitter.addListener('onAttributionChangeListener', val => {
+    this.onAttributionChangeListener(val);
+  });
+  DeviceEventEmitter.addListener('onDeeplinkResponseListener', val => {
+    this.onDeeplinkResponseListener(val);
+  });
+  DeviceEventEmitter.addListener('onReceiveUserIdListener', val => {
+    this.onReceiveUserIdListener(val);
+  });
+  DeviceEventEmitter.addListener('onSessionIdListener', val => {
+    this.onSessionIdListener(val);
+  });
 };
 
 MetrixConfig.prototype.setAppSecret = function(
@@ -20,122 +38,123 @@ MetrixConfig.prototype.setAppSecret = function(
   info1,
   info2,
   info3,
-  info4
+  info4,
 ) {
-  this.appSecret = {
+  this.settings.appSecret = {
     secretId: secretId.toString(),
     info1: info1.toString(),
     info2: info2.toString(),
     info3: info3.toString(),
-    info4: info4.toString()
+    info4: info4.toString(),
   };
 };
 
-MetrixConfig.prototype.setShouldLaunchDeeplink = function(
-  shouldLunchDeeplink
-) {
-  this.shouldLunchDeeplink = shouldLunchDeeplink;
+MetrixConfig.prototype.setShouldLaunchDeeplink = function(shouldLunchDeeplink) {
+  this.settings.shouldLunchDeeplink = shouldLunchDeeplink;
 };
 
 MetrixConfig.prototype.setLocationListening = function(locationListening) {
-  this.locationListening = locationListening;
+  this.settings.locationListening = locationListening;
 };
 
 MetrixConfig.prototype.setEventUploadThreshold = function(
-  eventUploadThreshold
+  eventUploadThreshold,
 ) {
-  this.eventUploadThreshold = eventUploadThreshold;
+  this.settings.eventUploadThreshold = eventUploadThreshold;
 };
 
 MetrixConfig.prototype.setEventUploadMaxBatchSize = function(
-  eventUploadMaxBatchSize
+  eventUploadMaxBatchSize,
 ) {
-  this.eventUploadMaxBatchSize = eventUploadMaxBatchSize;
+  this.settings.eventUploadMaxBatchSize = eventUploadMaxBatchSize;
 };
 
 MetrixConfig.prototype.setEventMaxCount = function(eventMaxCount) {
-  this.eventMaxCount = eventMaxCount;
+  this.settings.eventMaxCount = eventMaxCount;
 };
 
 MetrixConfig.prototype.setEventUploadPeriodMillis = function(
-  eventUploadPeriodMillis
+  eventUploadPeriodMillis,
 ) {
-  this.eventUploadPeriodMillis = eventUploadPeriodMillis;
+  this.settings.eventUploadPeriodMillis = eventUploadPeriodMillis;
 };
 
 MetrixConfig.prototype.setSessionTimeoutMillis = function(
-  sessionTimeoutMillis
+  sessionTimeoutMillis,
 ) {
-  this.sessionTimeoutMillis = sessionTimeoutMillis;
+  this.settings.sessionTimeoutMillis = sessionTimeoutMillis;
 };
 
 MetrixConfig.prototype.setEnableLogging = function(enableLogging) {
-  this.enableLogging = enableLogging;
+  this.settings.enableLogging = enableLogging;
 };
 
 MetrixConfig.prototype.setLogLevel = function(logLevel) {
-  this.logLevel = logLevel;
+  this.settings.logLevel = logLevel;
 };
 
 MetrixConfig.prototype.setFlushEventsOnClose = function(flushEventsOnClose) {
-  this.flushEventsOnClose = flushEventsOnClose;
+  this.settings.flushEventsOnClose = flushEventsOnClose;
 };
 
 MetrixConfig.prototype.setDefaultTrackerToken = function(defaultTrackerToken) {
-  this.defaultTrackerToken = defaultTrackerToken;
+  this.settings.defaultTrackerToken = defaultTrackerToken;
 };
 
 MetrixConfig.prototype.setStore = function(store) {
-  this.store = store;
+  this.settings.store = store;
 };
 
 MetrixConfig.prototype.setOnAttributionChangeListener = function(
-  onAttributionChangeListener
-) {    
+  onAttributionChangeListener,
+) {
+  this.settings.onAttributionChangedListener = true;
   this.onAttributionChangeListener = onAttributionChangeListener;
 };
 
 MetrixConfig.prototype.setOnDeeplinkResponseListener = function(
-  onDeeplinkResponseListener
+  onDeeplinkResponseListener,
 ) {
+  this.settings.onDeeplinkResponseListener = true;
   this.onDeeplinkResponseListener = onDeeplinkResponseListener;
 };
 
-MetrixConfig.prototype.setOnSessionIdListener = function(
-  onSessionIdListener
-) {
+MetrixConfig.prototype.setOnSessionIdListener = function(onSessionIdListener) {
+  this.settings.onSessionIdListener = true;
   this.onSessionIdListener = onSessionIdListener;
 };
 
 MetrixConfig.prototype.setOnReceiveUserIdListener = function(
-  onReceiveUserIdListener
+  onReceiveUserIdListener,
 ) {
+  this.settings.onReceiveUserIdListener = true;
   this.onReceiveUserIdListener = onReceiveUserIdListener;
 };
 
-
-module.exports = { Metrix, MetrixConfig,
-	initialize: MetrixUtil.initialize,
-	enableLocationListening: MetrixUtil.enableLocationListening,
-	disableLocationListening: MetrixUtil.disableLocationListening,
-	setEventUploadThreshold: MetrixUtil.setEventUploadThreshold,
-	setEventUploadMaxBatchSize: MetrixUtil.setEventUploadMaxBatchSize,
-	setEventMaxCount: MetrixUtil.setEventMaxCount,
-	setEventUploadPeriodMillis: MetrixUtil.setEventUploadPeriodMillis,
-	setSessionTimeoutMillis: MetrixUtil.setSessionTimeoutMillis,
-	enableLogging: MetrixUtil.enableLogging,
-	setLogLevel: MetrixUtil.setLogLevel,
-	setFlushEventsOnClose: MetrixUtil.setFlushEventsOnClose,
-	newEvent: MetrixUtil.newEvent,
-	newRevenue: MetrixUtil.newRevenue,
-	screenDisplayed: MetrixUtil.screenDisplayed,
-	setScreenFlowsAutoFill: MetrixUtil.setScreenFlowsAutoFill,
-	setMetrixApiKey: MetrixUtil.setMetrixApiKey,
-	setDefaultTracker: MetrixUtil.setDefaultTracker,
-	getSessionNum: MetrixUtil.getSessionNum,
-	isScreenFlowsAutoFill: MetrixUtil.isScreenFlowsAutoFill,
-	addUserAttributes: MetrixUtil.addUserAttributes,
-	setUserMetrics: MetrixUtil.setUserMetrics,
-	setAppSecret: MetrixUtil.setAppSecret,
-	setOnAttributionChangedListener:MetrixUtil.setOnAttributionChangedListener
+module.exports = {
+  Metrix,
+  MetrixConfig,
+  initialize: MetrixUtil.initialize,
+  enableLocationListening: MetrixUtil.enableLocationListening,
+  disableLocationListening: MetrixUtil.disableLocationListening,
+  setEventUploadThreshold: MetrixUtil.setEventUploadThreshold,
+  setEventUploadMaxBatchSize: MetrixUtil.setEventUploadMaxBatchSize,
+  setEventMaxCount: MetrixUtil.setEventMaxCount,
+  setEventUploadPeriodMillis: MetrixUtil.setEventUploadPeriodMillis,
+  setSessionTimeoutMillis: MetrixUtil.setSessionTimeoutMillis,
+  enableLogging: MetrixUtil.enableLogging,
+  setLogLevel: MetrixUtil.setLogLevel,
+  setFlushEventsOnClose: MetrixUtil.setFlushEventsOnClose,
+  newEvent: MetrixUtil.newEvent,
+  newRevenue: MetrixUtil.newRevenue,
+  screenDisplayed: MetrixUtil.screenDisplayed,
+  setScreenFlowsAutoFill: MetrixUtil.setScreenFlowsAutoFill,
+  setMetrixApiKey: MetrixUtil.setMetrixApiKey,
+  setDefaultTracker: MetrixUtil.setDefaultTracker,
+  getSessionNum: MetrixUtil.getSessionNum,
+  isScreenFlowsAutoFill: MetrixUtil.isScreenFlowsAutoFill,
+  addUserAttributes: MetrixUtil.addUserAttributes,
+  setUserMetrics: MetrixUtil.setUserMetrics,
+  setAppSecret: MetrixUtil.setAppSecret,
+  setOnAttributionChangedListener: MetrixUtil.setOnAttributionChangedListener,
 };
