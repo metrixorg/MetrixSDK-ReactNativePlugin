9e65332733d49a0416af3558d53a43a5067b07d5
Matin <m.zareimatin@gmail.com>
2020-04-05 04:48:01 +0430

    version update

--- src/metrix-util.js
+++ src/metrix-util.js
@@ -4,7 +4,7 @@ import { Platform } from "react-native";
 if (Platform.OS === "android") {
   module.exports = {
     initialize: function(appKey) {
-      Metrix.initialize(appKey, "0.14.6");
+      Metrix.initialize(appKey, "0.14.7");
     },
     enableLocationListening: function() {
       Metrix.enableLocationListening();
