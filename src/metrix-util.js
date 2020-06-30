8b7caad9c1ffb9bd9ac4ece5976c8666d72cb839
metrixCI <admin@metrix.ir>
2020-06-30 11:31:54 +0000

    Android version update to v0.15.5

--- src/metrix-util.js
+++ src/metrix-util.js
@@ -4,7 +4,7 @@ import { Platform } from "react-native";
 if (Platform.OS === "android") {
   module.exports = {
     initialize: function(appKey) {
-      Metrix.initialize(appKey, "0.15.4");
+      Metrix.initialize(appKey, "0.15.5");
     },
     enableLocationListening: function() {
       Metrix.enableLocationListening();
